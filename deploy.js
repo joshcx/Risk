2c219072f02b1d4826cd675ce871d4717e51c96e
SangHyuk Cho <hyukssang@gmail.com>
2015-12-01 18:12:30 -0500

    Update #4
    - Players take turns now
    - Icons added
    - Total number of troop deployed displayed

--- deploy.js
+++ deploy.js
@@ -1,8 +1,45 @@
+var gameplay = {
+
+}
 $('#playButton').click(function(){
 	$('.territory').show();
+	$('.message').show();
+	$('.icon').show();
+	$('.player').show();
+	$('#player1').css('color', 'blue');
+	gameplay.player = "player1";
 });
 $('.territory').click(function(){
-	var tmp = $("#"+this.id+" p").text();
+	if(gameplay.player == "player1"){
+		if($(this).hasClass('p2')){
+			alert("You clicked on the enemy territory!");
+			return;
+		}
+	}
+	else if(gameplay.player == "player2"){
+		if($(this).hasClass('p1')){
+			alert("You clicked on the enemy territory!");
+			return;
+		}
+	}
+	gameplay.clicked = $("#"+this.id+" p");
+	gameplay.deployed = $("#"+gameplay.player+" span");
+});
+$('#plusButton').click(function(){
+	var tmp = gameplay.clicked.text();
 	tmp++;
-	$("#"+this.id+" p").text(tmp);
-})
+	gameplay.clicked.text(tmp);
+
+	tmp = parseInt(gameplay.deployed.text());
+	tmp--;
+	gameplay.deployed.text(tmp);
+});
+$('#minusButton').click(function(){
+	var tmp = gameplay.clicked.text();
+	tmp--;
+	gameplay.clicked.text(tmp);
+
+	tmp = parseInt(gameplay.deployed.text());
+	tmp++;
+	gameplay.deployed.text(tmp);
+});
