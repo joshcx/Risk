b9ae4022fb1c06241b1b8a1a5d490c4ff082efb2
SangHyuk Cho <hyukssang@gmail.com>
2015-12-09 16:06:53 -0500

    Update #7
    Text added

--- map.js
+++ map.js
@@ -1,74 +1,43 @@
+
+
 var svg;
 var doc;
 var map;
 var label;
 var text;
-var hilite;
-var countries;
-var seas;
+var countries = [];
 function init( evt )
 {
   svg = evt.target;
   doc = svg.ownerDocument;
   selected = null;
   map = doc.getElementById( 'map' );
-  label = newElement( 'text', 'id=label font-size=30 stroke=none fill=black text-insert=middle x=100 y=700' );
-  text = doc.createTextNode( 'Click a country' );
-  label.appendChild( text );
-  svg.appendChild( label );
-  hilite = doc.getElementById( "hilite" );
-  // countries = document.getElementsByClassName('country');
-  var countries = [];
   $(".country").each(function(){
-    $(this).troops = 0;
-    $(this).owner = "None";
-    $(this).on({
-      mouseover: function(){
+    $(this).on({  
+      mouseover: function(e){
+        var country = e.target;
+        country.owner = "None";
+        country.troops = 0;
         var d = $(this).attr('d');
         $('#hovering').attr('d', d);
-        $('#status').text($(this).attr('id') + '// Owner: ' + $(this).owner + ' Troops: ' + $(this).troops);
-        
+        $('#dataDisplay').text(country.getAttribute('id') + " | Owner: " + country.owner + " | Troops: " + country.troops);
       }
     });
     $('#hovering').click(function(){
       var d = $(this).attr('d');
       $('#clicked').attr('d', d);
     });
+
   });
-  // $('.country').on({
-  //     mouseover: function(){
-  //       var tmp = $(this).attr('d');
-  //       $('#clicked')[0].attr('d', tmp);
-  //       $('#hovering')[0].attr('d', tmp);
-  //     },
-  //     mouseleave: function(){
-  //       $('#hovering').removeAttr('d');
-  //     },
-  //     click: function(){
-  //       $(this).off('mouseleave');
-  //     } 
-  //   });
-  // for (var i = 0; i < countries.length; i++)
-  // {
-  //   country = countries[i];
-  //   country.troops = 0;
-  //   country.owner = 'None';
-  //   country.addEventListener('mousedown', function(evt){ 
-  //     clickCountry(evt);
-  //   });
-  //   country.addEventListener('mouseover', function(evt){
-  //     hoverCountry(evt);
-  //   });
-  // }
-  seas = document.getElementsByClassName('sea');
-  for (var i = 0; i < seas.length; i++)
-  {
-    var sea = seas[i];
-    sea.addEventListener('click', function(evt){ 
-      clickSea(evt);
+    $(".sea").each(function(){
+      $(this).click(function(){
+        $('#hovering').removeAttr('d');
+        $('#clicked').removeAttr('d');
+      });
     });
-  }
+
 }
+
 // generic function to create an xml element
 // format for attr is very strict
 // attrs is a string of attribute=value pairs separated by single spaces, 
@@ -88,28 +57,5 @@ function newElement( type, attrs )
     }
   }
   return result;
-}
-function clickSea( evt )      
-{
-  var sea = evt.target;
-  text.textContent = sea.getAttribute( 'id' );
-  $('#clicked').attr( 'd', 'm0 0' );
-  selected = null;
-}
-function selectCountry( evt )      
-{
-  var country = evt.target;
-  var outline = country.getAttribute( 'd' );
-  $('#clicked').attr( "d", outline );
-  
-  selected = country;
-}
-function deselectCountry(evt)
-{
-  var country = evt.target;
-  var outline = country.getAttribute( 'd' );
-  $('#clicked').removeAttr( "d" );
-  text.textContent = country.getAttribute( 'id' ) +' Owner: ' +country.owner +' Troops: ' + country.troops;
-  selected = country;
 }
 
